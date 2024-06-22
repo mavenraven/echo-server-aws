19cb41e20f54780c715e8528324c24008f2d9fc5
mavenraven <developers@mavenraven.org>
2024-06-21 22:09:25 -0400

    --wip-- [skip ci]

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_ecr_repository" "echo_server_repo" {
+  name = "echo_server"
+}
+
+resource "aws_iam_role" "codebuild_role" {
+  name = "CodeBuild"
+  max_session_duration = 60 * 60
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = {
+        Service = "codebuild.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild_policy" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
+}
+
+
