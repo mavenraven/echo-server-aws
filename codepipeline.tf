13e4fdb8ec11c4b4debb360facfb1c5986e431c0
mavenraven <developers@mavenraven.org>
2024-06-22 17:30:38 -0400

    grabbing code from github via pipeline!

--- codepipeline.tf
+++ codepipeline.tf
@@ -2,6 +2,7 @@
 resource "aws_s3_bucket" "codepipeline_bucket" {
   bucket = "codepipeline-artifacts-${data.aws_caller_identity.current.account_id}"
   force_destroy = true
+
 }
 
 resource "aws_iam_role" "codepipeline_role" {
@@ -19,10 +20,41 @@ resource "aws_iam_role" "codepipeline_role" {
   })
 }
 
+#TODO: redudant now probably
 resource "aws_iam_role_policy_attachment" "codepipeline_full_access_policy" {
   role       = aws_iam_role.codepipeline_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSCodePipeline_FullAccess"
 }
+
+data "aws_iam_policy_document" "codepipeline_s3_policy_document" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetBucketVersioning",
+      "s3:PutObjectAcl",
+      "s3:PutObject",
+    ]
+
+    resources = [
+      aws_s3_bucket.codepipeline_bucket.arn,
+      "${aws_s3_bucket.codepipeline_bucket.arn}/*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "codepipline_s3_policy" {
+  name = "CodePipelineS3Policy"
+  policy = data.aws_iam_policy_document.codepipeline_s3_policy_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "codepipeline_s3_policy_attachment" {
+  role       = aws_iam_role.codepipeline_role.name
+  policy_arn = aws_iam_policy.codepipline_s3_policy.arn
+}
+
 
 resource "aws_codestarconnections_connection" "echo_server_codestarconnection" {
   name          = "echo_server_codestarconnection"
