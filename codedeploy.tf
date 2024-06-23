c3312cc2a94e7ff79f65d8bf39122c9e8cdad559
mavenraven <developers@mavenraven.org>
2024-06-23 18:04:53 -0400

    --wip-- [skip ci]

--- codedeploy.tf
+++ codedeploy.tf
@@ -4,12 +4,35 @@ resource "aws_codedeploy_app" "codedeploy_app" {
 
 }
 
-resource "aws_codedeploy_deployment_config" "foo" {
-  deployment_config_name = "echo_server_config"
+data "aws_iam_policy_document" "codedeploy_policy_document" {
+  statement {
+    effect = "Allow"
 
-  compute_platform = "ECS"
+    principals {
+      type        = "Service"
+      identifiers = ["codedeploy.amazonaws.com"]
+    }
 
-  traffic_routing_config {
-    type = "AllAtOnce"
+    actions = ["sts:AssumeRole"]
   }
+}
+
+resource "aws_iam_role" "codedeploy_iam_role" {
+  name               = "CodeDeploy"
+  assume_role_policy = data.aws_iam_policy_document.codedeploy_policy_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "aws_code_deploy_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
+  role       = aws_iam_role.codedeploy_iam_role.name
+}
+
+
+resource "aws_codedeploy_deployment_group" "codedeploy_deployment_group" {
+  app_name = aws_codedeploy_app.codedeploy_app.name
+  deployment_group_name = "echo_server_deployment_group"
+  service_role_arn = aws_iam_role.codedeploy_iam_role.arn
+  deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"
+
+
 }
